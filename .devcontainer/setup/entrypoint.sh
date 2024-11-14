123d6ba55a00b6cd0b97a2a8ab2af1144c07653f
Mathieu GARNIER <mgarnier11@gmail.com>
2024-11-14 08:54:36 +0100

    feat: added vscode web installation

--- .devcontainer/setup/entrypoint.sh
+++ .devcontainer/setup/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 sudo sh -c "dockerd >/var/log/dockerd.log 2>&1 &"
-sudo service ssh start
+
+bash /setup/setup-vscode.sh "$(tr '\n' ' ' </setup/vscode-params.json)"
 
 zsh /setup/setup.sh
+
+sudo service ssh start
 
 sleep infinity
