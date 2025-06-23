4727468931214a3201eaba8794b76a64ccdb1ca2
Mathieu GARNIER <mgarnier11@gmail.com>
2025-06-23 22:16:32 +0200

    devcontaienr base should be working good

--- .devcontainer/fish/config.fish
+++ .devcontainer/fish/config.fish
@@ -1,12 +1,9 @@
+#!/usr/bin/env fish
+
 if status is-interactive
-    set -x ZEPHYR_HOST ssh://mgarnier@100.64.98.97
-    set -x ATLAS_HOST ssh://mgarnier@100.64.98.98
-    set -x APOLLON_HOST ssh://mgarnier@100.64.98.99
-    set -x ATHENA_HOST ssh://mgarnier@100.64.98.100
+    # execute the script ~/.container-config.fish if it exists
+    if test -f ~/.container-config.fish
+        source ~/.container-config.fish
+    end
 
-    set -x COMPOSE_DIR /mnt/docker-data/zephyr/orchestrator
-    set -x ENV_DIR /mnt/docker-data/zephyr/orchestrator
-
-    home-cli completion fish | source
-    task --completion fish | source
 end
