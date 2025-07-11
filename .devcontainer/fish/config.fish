7db9f0ded549061545a3559dd6e18365a4d5d66a
mgarnier11 <mgarnier11@gmail.com>
2025-07-11 22:12:30 +0000

    feat: removed fish to readd zsh

--- .devcontainer/fish/config.fish
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env fish
-
-if status is-interactive
-    # execute the script ~/.container-config.fish if it exists
-    if test -f ~/.container-config.fish
-        source ~/.container-config.fish
-    end
-
-end
---
+++ .devcontainer/setup/_setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+. ~/.zshrc
+
+cp /run/secrets/SSH_PRIVATE_KEY ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+
+git config --global core.autocrlf false
+git config --global user.email "$GIT_EMAIL"
+git config --global user.name "$GIT_NAME"
+
+# install node
+node_version=${NODE_VERSION:-22}
+nvm install $node_version
+nvm use $node_version
