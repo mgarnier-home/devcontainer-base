30a0947b79337209a490da33fbed1ae788ad350b
Mathieu GARNIER <mgarnier11@gmail.com>
2025-10-01 21:30:15 +0200

    wip

--- .devcontainer/setup/00-setup.sh
+++ .devcontainer/setup/00-setup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env zsh
 . ~/.zshrc
 
+set -euxo pipefail
+
 git config --global core.autocrlf false
 git config --global user.email "$GIT_EMAIL"
 git config --global user.name "$GIT_NAME"
