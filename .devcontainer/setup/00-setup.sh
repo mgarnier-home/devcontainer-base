f139c5eecf7b36b2bbe9554b2c9086beca3873ab
Mathieu GARNIER <mgarnier11@gmail.com>
2025-10-01 21:22:01 +0200

    fix: dont asdf install globally

--- .devcontainer/setup/00-setup.sh
+++ .devcontainer/setup/00-setup.sh
@@ -4,8 +4,3 @@
 git config --global core.autocrlf false
 git config --global user.email "$GIT_EMAIL"
 git config --global user.name "$GIT_NAME"
-
-# install node
-node_version=${NODE_VERSION:-22.19.0}
-asdf install nodejs $node_version
-asdf global nodejs $node_version
